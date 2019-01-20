43b9e753d7a7204d180cf428bbdb6ff6c0cdbfaf
Scot Reichman <i2097i@hotmail.com>
2019-01-20 17:52:08 -0500

    Working on attaching signals

--- lib/gluby-tk/generator.rb
+++ lib/gluby-tk/generator.rb
@@ -1,30 +1,9 @@
 require 'stringio'
 require 'nokogiri'
+require 'yaml'
 
 module GlubyTK
-
   class Generator
-    DIR_ASSETS = "assets"
-    DIR_ASSETS_IMAGES = "#{DIR_ASSETS}/images"
-    DIR_INTERFACE = "interface"
-    DIR_SRC = "src"
-    DIR_SRC_GLUBY = "#{DIR_SRC}/gluby"
-
-    DIRECTORIES = [
-      DIR_ASSETS,
-      DIR_ASSETS_IMAGES,
-      DIR_INTERFACE,
-      DIR_SRC,
-      DIR_SRC_GLUBY
-    ]
-
-    TEMPLATES = [
-      {:name => "main.rb", :path => nil},
-      {:name => "includes.rb", :path => "src"},
-      {:name => "application.rb", :path => "src"},
-      {:name => "ApplicationWindow.glade", :path => "interface"}
-    ]
-
     SAMPLE_CODE_LINES = [
       "# Label Reference: https://ruby-gnome2.osdn.jp/hiki.cgi?pango-markup",
       "welcome_label.margin = 100",
@@ -49,7 +28,7 @@ module GlubyTK
       # Create gluby-tkrc file
       File.open("#{root}/.gluby-tkrc", "w+") { |file|
         file.write(module_name)
-      } 
+      }
 
       # Create sub-directories
       DIRECTORIES.each do |dir|
@@ -57,7 +36,7 @@ module GlubyTK
         Dir.mkdir "#{root}/#{dir}"
       end
 
-      # Generate files from templates      
+      # Generate files from templates
       TEMPLATES.each do |template|
         template_dest_file = "#{root}/#{template[:path].nil? ? "" : template[:path] + "/"}#{template[:name]}"
         GlubyTK.gputs "Creating #{template_dest_file}"
@@ -65,7 +44,7 @@ module GlubyTK
           file.write(get_template_contents("#{template[:name]}.template").gsub("gluby-tk_app_id", module_name))
         }
       end
-      
+
       # Construct gresource file & ruby classes
       rebuild(root)
 
@@ -80,31 +59,31 @@ module GlubyTK
       GlubyTK.gputs "Constructing gresource file..."
       root = root || Dir.pwd
 
-      interface_files = Dir["#{root}/interface/*.glade"]
+      interface_files = Dir["#{root}/#{DIR_INTERFACE}/*.glade"]
 
-      gluby_class_matcher = "#{root}/src/gluby/gluby_*.rb"
+      gluby_class_matcher = "#{root}/#{DIR_SRC_GLUBY}/gluby_*.rb"
       system("rm #{gluby_class_matcher}") if Dir[gluby_class_matcher].any?
-      
+
       gresource_file_contents = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
       gresource_file_contents += "<gresources>\n"
-      
+
       interface_files.each { |filename|
         gresource_file_contents += "<gresource prefix=\"/app/#{current_app_name(root)}\">"
         gresource_file_contents += "<file preprocess=\"xml-stripblanks\">#{File.basename(filename)}</file>"
         gresource_file_contents += "</gresource>"
         generate_ruby_class_for_document(File.basename(filename), File.read(filename), root)
       }
-      
+
       gresource_file_contents += "</gresources>\n"
 
-      File.open("#{root}/interface/interface.gresource.xml", "w+") { |file|
+      File.open("#{root}/#{DIR_INTERFACE}/interface.gresource.xml", "w+") { |file|
         file.write(Nokogiri::XML(gresource_file_contents, &:noblanks))
       }
     end
 
     def self.generate_ruby_class_for_document(file_name, file_contents, root)
       doc = Nokogiri::XML(file_contents)
-      
+
       if doc.css("template").first.nil?
         main_template = doc.css("object").first
         class_name = main_template["id"]
@@ -114,15 +93,14 @@ module GlubyTK
         class_name = main_template["class"]
         parent = main_template["parent"]
       end
-      
+
       parent = parent.gsub("Gtk","")
 
       base_file_name = "#{file_name.gsub(File.extname(file_name), "").underscore}"
 
       GlubyTK.gputs "Constructing #{base_file_name} Ruby class..."
 
-      gluby_file_dir = "#{root}/src/gluby"
-      gluby_file_path = "#{gluby_file_dir}/gluby_#{base_file_name}.rb"
+      gluby_file_path = "#{DIR_SRC_GLUBY}/gluby_#{base_file_name}.rb"
       gluby_class_file_contents = [
         "class #{class_name.underscore.humanize} < Gtk::#{parent}",
         "\ttype_register",
@@ -132,16 +110,38 @@ module GlubyTK
         "\t\t\t#{doc.css("[id]").map{|e| e.attributes["id"].value.to_sym }.select{|e| e != class_name.to_sym }}.each{|child_id| bind_template_child(child_id)}",
         "\t\tend",
         "\tend",
+      ]
+
+      gluby_class_file_contents += [
+        "",
+        "\tdef initialize"
+      ]
+
+      doc.css("[handler]").each do |handler_element|
+        element_name = handler_element.parent.attributes["id"]&.value
+        next if !element_name
+        signal_type = handler_element.attributes["name"].value
+        handler_method_name = handler_element.attributes["handler"].value
+
+        gluby_class_file_contents += [
+          "\t\tself.#{element_name}.signal_connect '#{signal_type}' do",
+          "\t\t\t#{handler_method_name}",
+          "\t\tend"
+        ]
+      end
+
+      gluby_class_file_contents += [
+        "\tend",
         "end"
-      ].join("\n")
+      ]
 
       File.open(gluby_file_path, "w+") { |file|
         GlubyTK.gputs "Writing #{gluby_file_path}..."
-        file.write gluby_class_file_contents
+        file.write gluby_class_file_contents.join("\n")
       }
 
       # TODO: Need to ensure that the user-facing class (not the gluby_class_name) reflects any updates such as root class name change etc.
-      file_path = "#{root}/src/#{base_file_name}.rb"
+      file_path = "#{root}/#{DIR_SRC}/#{base_file_name}.rb"
       app_name = current_app_name(root).humanize
       if !File.exist?(file_path)
         GlubyTK.gputs "#{file_path} did not exist. Creating..."
@@ -164,7 +164,40 @@ module GlubyTK
         }
       end
 
-      File.open("#{gluby_file_dir}/gluby_includes.rb", "a+") { |file|
+      # Connect signal handlers here
+
+      # gives us the element name
+      # doc.css("[handler]").first.parent.attributes["id"].value
+
+      # gives us the signal type
+      # doc.css("[handler]").first.attributes["name"].value
+
+      # gives us the handler method name
+      # doc.css("[handler]").first.attributes["handler"].value
+
+      doc.css("[handler]").each do |handler_element|
+        element_name = handler_element.parent.attributes["id"]&.value
+        next if !element_name
+        signal_type = handler_element.attributes["name"].value
+        handler_method_name = handler_element.attributes["handler"].value
+        if !File.read(file_path).include? "def #{handler_method_name}"
+          File.open(file_path, "a") { |file|
+            GlubyTK.gputs "Appending '#{signal_type}' signal handler method #{handler_method_name} for #{element_name} to #{File.basename(file_path)}..."
+            handler_text = "handler for #{element_name} signal #{signal_type} in #{File.basename(file_path)}'"
+            file.puts [
+              "\n",
+              "class #{class_name.underscore.humanize}",
+              "\t# #{handler_text}",
+              "\tdef #{handler_method_name}",
+              "\t\tputs 'running #{handler_text}",
+              "\tend",
+              "end"
+            ].join("\n")
+          }
+        end
+      end
+
+      File.open("#{DIR_SRC_GLUBY}/gluby_includes.rb", "a+") { |file|
         contents = file.read
         g_req = "require 'gluby_#{base_file_name}'"
         req = "require '#{base_file_name}'"
@@ -191,14 +224,46 @@ module GlubyTK
 
     def self.is_glubytk_directory?(dir = nil)
       is_g_project = File.exist?(glubytk_rc_path(dir))
-      
+
       GlubyTK.gputs "No GlubyTK project detected. Please make sure you are in the right directory." if !is_g_project
-      
+
       return is_g_project
     end
 
     def self.glubytk_rc_path(dir = nil)
       "#{dir || Dir.pwd}/.gluby-tkrc"
     end
+
+    def self.get_config
+      config_file = "config.yml"
+      if !File.exists?(config_file)
+        `cp #{template_dir}/#{config_file}.template ./#{config_file}`
+      end
+
+      YAML.load_file(config_file)
+    end
+
+    directories = GlubyTK::Generator.get_config["directories"]
+    DIR_ASSETS = directories["assets"] || "assets"
+    DIR_ASSETS_IMAGES = "#{DIR_ASSETS}/images"
+    DIR_INTERFACE = directories["interface"] || "interface"
+    DIR_SRC = directories["src"] || "src"
+    DIR_SRC_GLUBY = "#{DIR_SRC}/gluby"
+
+    DIRECTORIES = [
+      DIR_ASSETS,
+      DIR_ASSETS_IMAGES,
+      DIR_INTERFACE,
+      DIR_SRC,
+      DIR_SRC_GLUBY
+    ]
+
+    TEMPLATES = [
+      {:name => "main.rb", :path => nil},
+      {:name => "config.yml", :path => nil},
+      {:name => "includes.rb", :path => DIR_SRC},
+      {:name => "application.rb", :path => DIR_SRC},
+      {:name => "ApplicationWindow.glade", :path => DIR_INTERFACE}
+    ]
   end
 end
